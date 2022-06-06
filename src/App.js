9b822e54cbffd6d4d668733aaab2bea719cd05bb
ganzj <ganzj45@gmail.com>
2022-06-07 01:39:12 +0800

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import memories from "./images/memories.png";
+import { Container, Grow, Grid } from "@material-ui/core";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from "./styles";
 import { useDispatch } from "react-redux";
 import { getPosts } from "./actions/posts";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
 	const [currentId, setCurrentId] = useState(null);
@@ -18,25 +18,7 @@ const App = () => {
 
 	return (
 		<Container maxWidth="lg">
-			<AppBar
-				className={classes.appBar}
-				position="static"
-				color="inherit"
-			>
-				<Typography
-					className={classes.heading}
-					variant="h2"
-					align="center"
-				>
-					Memories
-				</Typography>
-				<img
-					className={classes.image}
-					src={memories}
-					alt="memories"
-					height="60"
-				/>
-			</AppBar>
+			<NavBar />
 			<Grow in>
 				<Container>
 					<Grid
